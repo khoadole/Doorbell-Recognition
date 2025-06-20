2cc830a8b2c1946779f2ef3e1272a4bc6a405bab
Huy Hoàng <cao.nguyen.huy.hoang@gmail.com>
2025-06-20 15:37:03 +0700

    Thêm sự kiện
    Thêm sự kiện đăng nhập: Khi người dùng đăng nhập, nếu đúng mật khẩu (giả sử là 123) thì sẽ được log vào trong home, khi đó mục profile sẽ đổi thành tên người dùng và không thể b5i thay đổi. Nút đăng nhập sẽ được thay thành nút đăng xuất. Sau khi đăng xuất thì nút đăng xuất sẽ trờ lại thành đăng nhập và profile sẽ trở về mặc định và username sẽ được đặt là Guest.

--- doorbell/urls.py
+++ doorbell/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('profile', views.profile, name='profile'),
     path('signin', views.signin, name='signin'),
     path('signup', views.signup, name='signup'),
+    path('signout', views.signout, name='signout'),
+
 ]
